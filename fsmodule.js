32e99f03a59018958ab180e0cba52af331d9b53d
Rahul Kumar Bisoyi <rahulkumarbisoyi31@gmail.com>
2026-08-18 09:30:30 +0530

    fsmodule

--- fsmodule.js
+++ fsmodule.js
@@ -7,7 +7,7 @@ async function createFile() {
     try {
         await fs.writeFile(
             fileName,
-            "Name: Satwik\nCourse: B.Tech CSE",
+            "Name: Rahul\nCourse: B.Tech CSE",
             "utf8"
         );
 
@@ -18,33 +18,33 @@ async function createFile() {
 }
 
 
-// // 2. READ
-// async function readFile() {
-//     try {
-//         const data = await fs.readFile(fileName, "utf8");
+// 2. READ
+async function readFile() {
+    try {
+        const data = await fs.readFile(fileName, "utf8");
 
-//         console.log("\nFile Content:");
-//         console.log(data);
-//     } catch (error) {
-//         console.log("Error:", error.message);
-//     }
-// }
+        console.log("\nFile Content:");
+        console.log(data);
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
 
 
-// // 3. UPDATE
-// async function updateFile() {
-//     try {
-//         await fs.appendFile(
-//             fileName,
-//             "\nCollege: ABES Engineering College............",
-//             "utf8"
-//         );
+// 3. UPDATE
+async function updateFile() {
+    try {
+        await fs.appendFile(
+            fileName,
+            "\nCollege: ABES Engineering College............",
+            "utf8"
+        );
 
-//         console.log("\nFile updated successfully");
-//     } catch (error) {
-//         console.log("Error:", error.message);
-//     }
-// }
+        console.log("\nFile updated successfully");
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
 
 
 // // 4. DELETE
@@ -63,9 +63,9 @@ async function main() {
 
     await createFile();
 
-    // await readFile();
+    await readFile();
 
-    // await updateFile();
+    await updateFile();
 
     // await deleteFile();
 }
